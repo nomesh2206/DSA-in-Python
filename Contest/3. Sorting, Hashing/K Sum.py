123f2aa2d5b39f496cabf8721aaf898c3d0e27e7
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2024-05-19 16:49:09 +0530

    Create K Sum.py
    You are given an array of integers. You have to find the sum of K smallest numbers

---
+++ Contest/3. Sorting, Hashing/K Sum.py
@@ -0,0 +1,9 @@
+def kSum(a,n,k):
+    #Your code here
+    a.sort()
+    total_sum = sum(a[0:k])
+    
+    return total_sum
+
+# That's it, you dont have to apply any sorting techinque manually
+# Or use any loop to find the sum 
